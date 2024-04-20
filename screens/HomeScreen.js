01b36cd5926d1c9232db99c62a474d738a568114
kimjason832 <kimjason.832@gmail.com>
2024-04-20 12:39:41 -0700

    Login Firestone Information

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { user, handleAuthentication } from './LoginScreen';
+// import { initializeApp } from '@firebase/app';
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from '@firebase/auth';
+// import LoginScreen from './screens/LoginScreen';
 
-const MyComponent = () => {
+const HomeScreen = ({ user, handleAuthentication }) => {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.authContainer}>
+      <Text style={styles.title}>Welcome to the Home Screen</Text>
+      <Button title="Logout" onPress={handleAuthentication} color="#e74c3c" />
     </View>
   );
 };
 
-export default MyComponent;
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#f0f0f0',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  authContainer: {
+    width: '80%',
+    maxWidth: 400,
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+    elevation: 3,
+  },
+});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,7 +3,7 @@ import { View, Text, TextInput, Button, StyleSheet, ScrollView, Alert } from 're
 import HomeScreen from './HomeScreen';
 import { initializeApp } from '@firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from '@firebase/auth';
-
+import { getFirestore, collection, addDoc } from '@firebase/firestore';
 const firebaseConfig = {
   apiKey: "AIzaSyDBU9pkEt4r13KFmXsR95N0jhFPfWHt_h4",
   authDomain: "casus-9a7da.firebaseapp.com",
@@ -15,12 +15,14 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // get firestore instance
 
-const AuthScreen = ({email, setEmail, password, setPassword, isLogin, setIsLogin, handleAuthentication}) => {
+const AuthScreen = ({ email, setEmail, password, setPassword, name, setName, isLogin, setIsLogin, handleAuthentication }) => {
   return (
     <View style={styles.authContainer}>
       <Text style={styles.title}>{isLogin ? 'Sign In' : 'Sign Up'}</Text>
-
+ 
+ 
       <TextInput
         style={styles.input}
         value={email}
@@ -28,6 +30,20 @@ const AuthScreen = ({email, setEmail, password, setPassword, isLogin, setIsLogin
         placeholder="Email"
         autoCapitalize="none"
       />
+ 
+ 
+      {/* Render name input only during sign-up */}
+      {!isLogin && (
+        <TextInput
+          style={styles.input}
+          value={name}
+          onChangeText={setName}
+          placeholder="Name"
+          autoCapitalize="none"
+        />
+      )}
+ 
+ 
       <TextInput
         style={styles.input}
         value={password}
@@ -35,48 +51,50 @@ const AuthScreen = ({email, setEmail, password, setPassword, isLogin, setIsLogin
         placeholder="Password"
         secureTextEntry
       />
-
+ 
+ 
       <View style={styles.buttonContainer}>
         <Button title={isLogin ? 'Sign In' : 'Sign Up'} onPress={handleAuthentication} color="#3498db" />
       </View>
-
+ 
+ 
       <View style={styles.bottomContainer}>
         <Text style={styles.toggleText} onPress={() => setIsLogin(!isLogin)}>
           {isLogin ? 'Need an account? Sign Up' : 'Already have an account? Sign In'}
         </Text>
       </View>
-
-      
+ 
+ 
     </View>
   );
-}
-
-const AuthenticatedScreen = ({ user, handleAuthentication }) => {
-  return (
-    <View style={styles.authContainer}>
-      <Text style={styles.title}>Welcome</Text>
-      <Text style={styles.emailText}>{user.email}</Text>
-      <Button title="Logout" onPress={handleAuthentication} color="#e74c3c" />
-    </View>
-  );
-};
-
-export default App = () => {
+ }
+ 
+ 
+ export default App = () => {
+  // State variables for email, password, name, user, and isLogin
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
   const [user, setUser] = useState(null); // Track user authentication state
-  const [isLogin, setIsLogin] = useState(true);
-
+  const [isLogin, setIsLogin] = useState(true); // Default to login mode
+ 
+ 
+  // Initialize Firebase app and authentication instance
   const auth = getAuth(app);
+ 
+ 
+  // useEffect to listen for authentication state changes
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
     });
-
+ 
+ 
     return () => unsubscribe();
   }, [auth]);
-
-  
+ 
+ 
+  // handleAuthentication function to handle sign-up and sign-in
   const handleAuthentication = async () => {
     try {
       if (user) {
@@ -84,37 +102,55 @@ export default App = () => {
         console.log('User logged out successfully!');
         await signOut(auth);
       } else {
-        // Sign in or sign up
-        if (isLogin) {
-          // Sign in
+        // Sign in or sign up based on isLogin state
+        if ( isLogin){
+          // Sign in with email and password
           await signInWithEmailAndPassword(auth, email, password);
           console.log('User signed in successfully!');
         } else {
-          // Sign up
+          // Sign up with email, password, and name
           await createUserWithEmailAndPassword(auth, email, password);
           console.log('User created successfully!');
+ 
+ 
+          // Save user data into Firestore collection
+          const userData = {
+            email: email,
+            username: name, // Include the user's name
+            // Add additional user data as needed
+          };
+ 
+ 
+          const docRef = await addDoc(collection(db, 'users'), userData);
+ 
+ 
+          console.log('User data saved with ID: ', docRef.id);
         }
       }
     } catch (error) {
+      // Handle authentication errors
       if (error.message === 'Firebase: Password should be at least 6 characters (auth/weak-password).') {
         Alert.alert('Error', 'Invalid password. Password should be at least 6 characters.');
       } else {
         Alert.alert('Error', error.message);
       }
+    }
   };
-  }
+ 
+ 
   return (
     <ScrollView contentContainerStyle={styles.container}>
+      {/* Render HomeScreen if user is authenticated, otherwise render AuthScreen */}
       {user ? (
-        // Show user's email if user is authenticated
         <HomeScreen user={user} handleAuthentication={handleAuthentication} />
       ) : (
-        // Show sign-in or sign-up form if user is not authenticated
         <AuthScreen
           email={email}
           setEmail={setEmail}
           password={password}
           setPassword={setPassword}
+          name={name}
+          setName={setName}
           isLogin={isLogin}
           setIsLogin={setIsLogin}
           handleAuthentication={handleAuthentication}
@@ -122,8 +158,8 @@ export default App = () => {
       )}
     </ScrollView>
   );
-}
-
+ }
+ 
 
 //TEMP PLZ CHANGE
 const styles = StyleSheet.create({
